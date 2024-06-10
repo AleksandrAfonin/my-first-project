579719882fac98645dc34eda8a97cfdccf8b20b2
Aleksandr Afonin <varsup@yandex.ru>
2024-06-10 14:14:04 +0300

    homework13

---
+++ src/main/java/ru/otus/java/basic/homework13/Main.java
@@ -0,0 +1,34 @@
+package ru.otus.java.basic.homework13;
+
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String userInput;
+    String result;
+
+    try (Socket socket = new Socket("localhost", 8089)) {
+      Client client = new Client(socket);
+      System.out.println("Server: " + client.read());
+      System.out.println("Enter 'exit' for exit");
+
+      while (true) {
+        System.out.print("Client: ");
+        userInput = scanner.nextLine();
+        if (userInput.equals("exit")) {
+          break;
+        }
+        client.send(userInput);
+        result = client.read();
+
+        System.out.println("Server: " + result);
+      }
+      System.out.println("Exit");
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
